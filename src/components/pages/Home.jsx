a9fed10d089b6ba2a690e15b2d06f0bb0b1b3839
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-07 02:17:16 +0600

    handleAddto Cart button functionality is done and is perfectly okay now

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import HomeData from './HomeData';
+import OrderReview from './OrderReview';
 
 const Home = () => {
     const data = useLoaderData();
+    const [cart, setCart] = useState([]);
 
+    const handleAddtoCart = (id) => {
+        setCart([...cart, id]);
+    };
+    console.log(cart);
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-4 justify-items-center gap-4">
-            {data.map((el) => (
-                <HomeData key={el._id} data={el} />
-            ))}
+        <div className="grid grid-cols-12 justify-items-center gap-4">
+            <div className="col-span-10 grid grid-cols-3 gap-4">
+                {data.map((el) => (
+                    <HomeData key={el._id} data={el} handleAddtoCart={handleAddtoCart} />
+                ))}
+            </div>
+
+            <div className="mt-14">
+                <OrderReview />
+            </div>
         </div>
     );
 };
--- src/components/pages/HomeData.jsx
+++ src/components/pages/HomeData.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
-const HomeData = ({ data }) => {
+const HomeData = ({ data, handleAddtoCart }) => {
     const { _id, price, picture, name } = data;
+
+    const handleBuyNow = (id) => {
+        handleAddtoCart(id);
+    };
     return (
         <div className="mt-14 border-2 border-orange-500 rounded-2xl shadow-2xl p-4">
             <img src={picture} alt={name} className="object-contain w-full h-52 rounded-3xl" />
             <h1 className="font-bold text-2xl text-violet-500 p-3">Name: {name}</h1>
             <p className="mt-auto font-bold text-xl text-red-500 p-3">price: {price}</p>
+
+            <button className="btn" onClick={() => handleBuyNow(_id)}>
+                Buy Now
+            </button>
         </div>
     );
 };
