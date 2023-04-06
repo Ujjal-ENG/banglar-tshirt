ee989f53d732c7ff8c5235b30896829cd6d597ac
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-07 02:46:34 +0600

    now i rendered the components using conditions and its called conditional rendering

--- src/components/pages/OrderReview.jsx
+++ src/components/pages/OrderReview.jsx
@@ -7,14 +7,21 @@ const OrderReview = ({ cart, handleDelete }) => {
     return (
         <div className=" text-center space-y-4 space-x-3">
             <span className="text-3xl font-bold text-orange-500">Order Summary: {cart.length}</span>
-            {cart.map((el) => (
-                <h3 key={el.id} className="font-bold text-xl text-violet-500">
-                    {el.name}
-                    <span className="btn ml-5 cursor-pointer" onClick={() => handleDeleted(el.id)}>
-                        X
-                    </span>
-                </h3>
-            ))}
+            <br />
+            {cart.length === 0 ? (
+                <span className="text-3xl font-bold text-yellow-500">Please add Some Products</span>
+            ) : (
+                <>
+                    {cart.map((el) => (
+                        <h3 key={el.id} className="font-bold text-xl text-violet-500">
+                            {el.name}
+                            <span className="btn ml-5 cursor-pointer" onClick={() => handleDeleted(el.id)}>
+                                X
+                            </span>
+                        </h3>
+                    ))}
+                </>
+            )}
         </div>
     );
 };
