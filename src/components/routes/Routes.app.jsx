35e75bc37d996c13c741650d62157cb6e5d91d4b
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-07 00:40:06 +0600

    setup our main homw routers

--- src/components/routes/Routes.app.jsx
+++ src/components/routes/Routes.app.jsx
@@ -1,34 +1,38 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "../../App";
-import About from "../pages/About";
-import ContactUs from "../pages/ContactUs";
-import Users from "../pages/Users";
-import UserDetails from "../pages/UserDetails";
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../../App';
+import About from '../pages/About';
+import ContactUs from '../pages/ContactUs';
+import Home from '../pages/Home';
+import UserDetails from '../pages/UserDetails';
+import Users from '../pages/Users';
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact-us",
-        element: <ContactUs />,
-      },
-      {
-        path: "users",
-        element: <Users />,
-        loader: async () => fetch("https://jsonplaceholder.typicode.com/users"),
-      },
-      {
-        path: "user-details/:id",
-        element: <UserDetails />,
-        loader: async ({ params }) =>
-          fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`),
-      },
-    ],
-  },
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/about',
+                element: <About />
+            },
+            {
+                path: '/contact-us',
+                element: <ContactUs />
+            },
+            {
+                path: 'users',
+                element: <Users />,
+                loader: async () => fetch('https://jsonplaceholder.typicode.com/users')
+            },
+            {
+                path: 'user-details/:id',
+                element: <UserDetails />,
+                loader: async ({ params }) => fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+            }
+        ]
+    }
 ]);
